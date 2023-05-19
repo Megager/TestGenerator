04827a70663ae7483042fa38dcdece34cd7cfeb8
DMZA <DMZA@UA01WP0475>
2023-05-19 14:35:46 +0300

    Add project files.

---
+++ Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("SimCorp")]
+[assembly: AssemblyProduct("Test")]
+[assembly: AssemblyCopyright("Copyright © SimCorp 2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("743fca4e-b28b-4bfd-91b2-421d6994f703")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
